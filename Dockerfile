b370821766f5a7f9e0822d457b7b1424c7689531
Aleff <aleff.a.s.o@gmail.com>
2022-06-18 12:52:00 -0300

    uploading config

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:latest
+
+WORKDIR /app
+
+ADD package.json /app
+
+RUN npm install
+
+ADD . /app
+
+EXPOSE 3000
+
+CMD ["npm", "run", "start"]
---
+++ db/db.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config({path: './.env'})
+
+const connection = new Sequelize(
+    process.env.DATABASE_TABLE, 
+    process.env.DATABASE_USER, 
+    process.env.DATABASE_PASSWORD,
+    { //Database set up
+        host: process.env.DATABASE_HOST, // Whenever you want to run in a server you can replace localhost with the IP address
+        dialect: process.env.DATABASE_DIALECT,
+        timezone: process.env.DATABASE_TIMEZONE
+    });
+
+module.exports = connection;
