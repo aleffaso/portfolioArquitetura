a5dfddc47749f4dd3b98947f2b87fc9dcfdde23a
Aleff <aleff.a.s.o@gmail.com>
2022-06-18 13:03:28 -0300

    database update

--- db/db.js
+++ db/db.js
@@ -5,12 +5,11 @@ dotenv.config({path: './.env'})
 
 const connection = new Sequelize(
     process.env.DATABASE_TABLE, 
-    process.env.DATABASE_USER, 
-    process.env.DATABASE_PASSWORD,
+    process.env.POSTGRES_USER, 
+    process.env.POSTGRES_PASSWORD,
     { //Database set up
         host: process.env.DATABASE_HOST, // Whenever you want to run in a server you can replace localhost with the IP address
         dialect: process.env.DATABASE_DIALECT,
-        timezone: process.env.DATABASE_TIMEZONE
     });
 
 module.exports = connection;
